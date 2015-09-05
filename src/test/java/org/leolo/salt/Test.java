4dc32a8f2351e6280ae65930cda7f6d906855a8d
LO Kam Tao Leo <leolo@leolo.org>
2015-09-05 22:23:22 +0800

    Standard Test Cases

--- src/test/java/org/leolo/salt/Test.java
+++ src/test/java/org/leolo/salt/Test.java
@@ -2,8 +2,37 @@ package org.leolo.salt;
 
 public class Test {
 	public static void main(String [] args){
-		for(int i=0;i<50;i++){
-			System.out.println(SaltGenerator.getInstance().getSalt(79));
-		}
+		String password = "tomcat";
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.MD5));
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.SHA1));
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.SHA256));
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.SHA384));
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.SHA512));
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.SALTED_MD5));
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.SALTED_SHA1));
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.SALTED_SHA256));
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.SALTED_SHA384));
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.SALTED_SHA512));
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.HMAC_MD5));
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.HMAC_SHA1));
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.HMAC_SHA256));
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.HMAC_SHA384));
+		System.out.print(password+" : ");
+		System.out.println(Salt.createHash(password, HashType.HMAC_SHA512));
+		System.out.println(Salt.verify(password, Salt.createHash(password, HashType.HMAC_SHA512)));
 	}
 }
