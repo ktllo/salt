710278c43a33e0eae4004027c6f6e255c40c676b
LO Kam Tao Leo <leolo@leolo.org>
2015-09-05 22:49:16 +0800

    Documents

--- src/main/java/org/leolo/salt/HashType.java
+++ src/main/java/org/leolo/salt/HashType.java
@@ -1,21 +1,70 @@
 package org.leolo.salt;
 
+/**
+ * Identify the hashing algorithm used
+ * @author leolo
+ *
+ */
 public enum HashType {
-	
+	/**
+	 * Plain MD5 hash. This option is considered as insecure
+	 */
 	MD5("mp"),
+	/**
+	 * Plain SHA-1 hash. This option is considered as insecure
+	 */
 	SHA1("s1"),
+	/**
+	 * Plain SHA-256 hash.
+	 */
 	SHA256("s2"),
+	/**
+	 * Plain SHA-384 hash.
+	 */
 	SHA384("s3"),
+	/**
+	 * Plain SHA-512 hash.
+	 */
 	SHA512("s5"),
+	/**
+	 * Salted MD5 hash.
+	 */
 	SALTED_MD5("md"),
+	/**
+	 * Salted SHA-1 hash.
+	 */
 	SALTED_SHA1("d1"),
+	/**
+	 * Salted SHA-256 hash.
+	 */
 	SALTED_SHA256("d2"),
+	/**
+	 * Salted SHA-384 hash.
+	 */
 	SALTED_SHA384("d3"),
+	/**
+	 * Salted SHA-512 hash.
+	 */
 	SALTED_SHA512("d5"),
+	/**
+	 * Keyed MD5 hash.
+	 */
 	HMAC_MD5("mh"),
+	/**
+	 * Keyed SHA-1 hash.
+	 */
 	HMAC_SHA1("h1"),
+	/**
+	 * Keyed SHA-256 hash.
+	 */
 	HMAC_SHA256("h2"),
+	/**
+	 * Keyed SHA-384 hash.
+	 */
 	HMAC_SHA384("h3"),
+	/**
+	 * Keyed SHA-512 hash.
+	 */
 	HMAC_SHA512("h5");
 	
 	
@@ -28,15 +77,15 @@ public enum HashType {
 	 */
 	private String id;
 
-	public String getId() {
+	String getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	void setId(String id) {
 		this.id = id;
 	}
 	
-	public static HashType getType(String id){
+	static HashType getType(String id){
 		for(HashType ht:HashType.values()){
 			if(id.equals(ht.id))
 				return ht;
